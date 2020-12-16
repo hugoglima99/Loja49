cc157c9cae0162c6e2d503aa2c2ea4dc9c5ced84
Hugo <Hugo.Goncalves.Lima@formandos.cinel.pt>
2020-12-16 09:55:13 +0000

    Inicializacao do projeto

---
+++ Loja49/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Loja49.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
