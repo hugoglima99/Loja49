069394e67004ace17d26ecb35e776c5deeabdb6d
Hugo <Hugo.Goncalves.Lima@formandos.cinel.pt>
2020-12-16 12:43:06 +0000

    Criar a base de dados e tabela produtos

---
+++ Loja49/Dados/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Loja49.Dados.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Loja49.Dados
+{
+    public class DataContext : DbContext
+
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Loja49/Dados/Entidades/Produto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja49.Dados.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        [DisplayFormat(DataFormatString = "{o:C2}", ApplyFormatInEditMode = false)]
+        public decimal Preco { get; set; }
+
+        [Display(Name = "Image")]
+        public string Url { get; set; }
+
+        [Display(Name = "Última Compra")]
+        public DateTime UltimaCompra { get; set; }
+
+        [Display(Name = "Última Venda")]
+        public DateTime UltimaVenda { get; set; }
+
+        [Display(Name = "Disponível")]
+        public bool Disponivel { get; set; }
+
+        [DisplayFormat(DataFormatString = "{o:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+
+
+    }
+}
